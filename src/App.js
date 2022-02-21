bc0c059b8f698c92f43a5a286b4cb3c6eafda12f
Jussi Wedman <juswedma@gmail.com>
2022-02-21 23:29:49 +0200

    asd

--- src/App.js
+++ src/App.js
@@ -4,6 +4,37 @@ import {useState} from 'react'
 const URL = 'http://api.exchangeratesapi.io/v1/latest?access_key='
 const API_KEY = 'fb79f871d5455254d57271af7fca0f1a'
 
+
+function App() {
+  const [eur, setEur] = useState(0);
+  const [gbp, setGbp] = useState(0);
+  const [rate, setRate] = useState(0);
+
+  return (
+    <div id="container">
+      <form onSubmit={convert}>
+        <div>
+          <label>Eur</label>&nbsp;
+          <input
+            type="number"
+            step="0.01"
+            value={eur}
+            onChange={(e) => setEur(e.target.value)}
+          ></input>
+          <output>{rate}</output>
+        </div>
+        <div>
+          <label>Gbp</label>
+          <output>{gbp.toFixed(2)} €</output>
+        </div>
+        <div>
+          <button>Calculate</button>
+        </div>
+      </form>
+    </div>
+  );
+}
+
 async function convert(e) {
   e.preventDefault();
   try {
@@ -15,37 +46,14 @@ async function convert(e) {
       console.log(json.rates.GBP);
       setRate(json.rates.GBP);
 
-      setGbp(eur * json.rates.GBP)
+      setGbp(eur * json.rates.GBP);
     } else {
-      alert('Error retrieving exchange rate.');
+      alert("Error retrieving exchange rate.");
       console.log(response);
     }
+  } catch (err) {
+    alert(err);
   }
-}
-
-function App() {
-  const [eur,setEur] = useState(0);
-  const [gbp,setGbp] = useState(0);
-  const [rate, setRate] = useState(0);
-  return (
-    <div id='container'>
-      <form onSubmit={convert}>
-        <div>
-          <label>Eur</label>&nbsp;
-          <input type='number' step="0.01" value={eur} onChange={e => setEur(e.target.value)}> </input>
-          <output>{rate}</output>
-        </div>
-        <div>
-          <label>Gdp</label>
-          <output>{gbp.toFixed(2)}€</output>
-        </div>
-        <div>
-          <button>Calculate</button>
-        </div>
-      </form>
-    </div>
-  
-  );
 }
 
 export default App;
